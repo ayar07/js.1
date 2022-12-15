295166fcd06a04378250082fe1e017e3dfa1eaff
ayar07 <ayar07@gmail.com>
2022-12-15 15:34:31 +0600

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-
-for (let i = 0; i <=20; i++) {
-    if (i % 2) {
-    } else {
-        console.log([i]);
-    }
-}
-
 var blacklist = ['ainazik', 'aktan', 'ariet', 'ruslan', 'tima',
         'AINAZIK', 'AKTAN', ' ARIET', 'RUSLAN', 'TIMA',
         'Ainazik', 'Aktan', 'Ariet', 'Ruslan', 'Tima']
@@ -18,7 +10,7 @@ do{
     if(blacklist.includes(name)) {
       name = prompt('Введите имя:')
     } else {
-        alert('Welcome');
+        alert(`Добро пожаловать ${name}`);
         break;
     }
     i++
